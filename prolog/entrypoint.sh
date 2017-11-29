a5a7709c9f75f5945dc94f3b8a8ab6e2fbc76c15
Andrew Rice <acr31@cam.ac.uk>
2017-11-29 16:50:51 +0000

    Updated x11 vm (not working with tester still) and added a prolog vm

--- prolog/entrypoint.sh
+++ prolog/entrypoint.sh
@@ -8,8 +8,4 @@ fi
 useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user
 export HOME=/home/user
 
-ln -s /usr/local/maven-cache/.m2 /home/user/.m2
-
-/usr/local/bin/gosu user /usr/bin/Xvfb :1 -screen 0 1024x768x16 >/dev/null 2>&1 &
-
 exec /usr/local/bin/gosu user "$@"
