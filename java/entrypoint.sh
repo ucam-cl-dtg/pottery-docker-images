abc972bf4e32fafe7dfd631c71b93c4c79e8fb3e
Andrew Rice <acr31@cam.ac.uk>
2017-12-31 16:47:07 +0000

    removed java alias from entrypoint

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -8,8 +8,6 @@ fi
 useradd --shell /bin/bash -u $LOCAL_USER_ID -o -c "" -m user
 export HOME=/home/user
 
-echo "alias java=\"java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap\"" >> /home/user/.bashrc
-
 ln -s /usr/local/maven-cache/.m2 /home/user/.m2
 
 exec /usr/local/bin/gosu user "$@"
